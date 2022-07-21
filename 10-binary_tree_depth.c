4956424eae765c48ff2781deefb70b3da39e4eb5
abdou50 <abdelkaderraddadi988@gmail.com>
2022-07-21 21:06:09 +0300

    l

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,15 +1,17 @@
 #include "binary_trees.h"
 /**
- * binary_tree_depth- insert a node as the left-child of another node
+ * binary_tree_depth - insert a node as the left-child of another node
  * @tree: a pointer to the node to insert the left-child in
  * *Return: pointer
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	int left = 0;
+	int d = 0;
 
-	if (tree == NULL)
-		return (0);
-	left = binary_tree_depth(tree->left) + binary_tree_depth(tree->right);
-	return (left  + 1);
+	while (tree->parent)
+	{
+		d++;
+		tree = tree->parent;
+	}
+	return (d);
 }
