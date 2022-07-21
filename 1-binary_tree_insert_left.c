c08184932eae2247fb36244732d6ea5809c290f6
khawladaouay <daouaykhawla782@gmail.com>
2022-07-21 17:54:39 +0100

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,7 +7,7 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	struct binary_tree_s *left_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *left_node = malloc(sizeof(binary_tree_t));
 
 	if (parent == NULL)
 	{
