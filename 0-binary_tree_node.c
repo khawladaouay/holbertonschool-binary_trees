b8bc4b8dcc5ab3efc74c52fe2d1b12170e33d91a
abdou50 <abdelkaderraddadi988@gmail.com>
2022-08-08 14:10:56 +0300

    message

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,16 +7,16 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-struct binary_tree_s *node = malloc(sizeof(binary_tree_t));
+	struct binary_tree_s *node = malloc(sizeof(binary_tree_t));
 
-if (node == NULL)
-{
-	return (NULL);
-}
+	if (node == NULL)
+	{
+		return (NULL);
+	}
 
-node->n = value;
-node->parent = parent;
-node->left = NULL;
-node->right = NULL;
-return (node);
+	node->n = value;
+	node->parent = parent
+					   node->left = NULL;
+	node->right = NULL;
+	return (node);
 }
